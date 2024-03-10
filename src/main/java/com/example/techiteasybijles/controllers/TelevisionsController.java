84d6121763322c26bd67f95ee1d56001010f84ff
FanqiangYe <fanqiang_ye@hotmail.com>
2024-03-10 16:05:14 +0100

    Part 3 stap 10

--- src/main/java/com/example/techiteasybijles/controllers/TelevisionsController.java
+++ src/main/java/com/example/techiteasybijles/controllers/TelevisionsController.java
@@ -1,30 +1,26 @@
 package com.example.techiteasybijles.controllers;
 
-import com.example.techiteasybijles.exceptions.RecordNotFoundException;
 import com.example.techiteasybijles.models.Television;
-import com.example.techiteasybijles.repositories.TelevisionRepository;
-import org.springframework.http.HttpStatus;
+import com.example.techiteasybijles.services.TelevisionService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
+//@RequestMapping("/televisions") -> dit had ook gekund voor de uri endpoint
 @RestController
 public class TelevisionsController {
 
-    private final TelevisionRepository televisionRepository;
+    private final TelevisionService televisionService;
 
-    public TelevisionsController(TelevisionRepository televisionRepository) {
-        this.televisionRepository = televisionRepository;
+    public TelevisionsController(TelevisionService televisionService) {
+        this.televisionService = televisionService;
     }
 
 
     @GetMapping("/televisions")
     public ResponseEntity <List<Television>> getAllTelevisions(){
-        return ResponseEntity.ok(televisionRepository.findAll());
+        return ResponseEntity.ok(televisionService.getTelevisions());
     }
 
     @GetMapping("/televisions/{id}")
@@ -32,62 +28,29 @@ public class TelevisionsController {
         // Haal de television met het gegeven id uit de database.
         // Het Optional datatype betekend "wel of niet". Als er geen television gevonden wordt, dan is de Optional empty,
         // maar als er wel een television gevonden wordt, dan staat de television in de Optional en kun je deze er uit
-        Optional<Television> television = televisionRepository.findById(id);
-        if (television.isEmpty()) {
-           throw new RecordNotFoundException("No television found with id: " + id);
-// Hoeft hier geen "else" te komen?
-        }
-        return ResponseEntity.ok().body(television.get());
+        Television television = televisionService.getTelevision(id);
+        return ResponseEntity.ok(television);
     }
 
     // Wanneer je een klasse definieert in een programmeertaal zoals Java, kun je die
     // klasse gebruiken als een nieuw datatype om objecten van dat type te maken.
     @PostMapping("/televisions")
-    public ResponseEntity<Void> addTelevision (@RequestBody Television television) {
-        televisionRepository.save(television);
-        return ResponseEntity.created(null).build();
+    public ResponseEntity<Television> addTelevision (@RequestBody Television television) {
+        televisionService.saveTelevision(television);
+        return ResponseEntity.created(null).body(television);
     }
 
     @PutMapping("/televisions/{id}")
-    public ResponseEntity<Television> updateTelevison(@RequestBody Television newTelevision,
+    public ResponseEntity<Void> updateTelevison(@RequestBody Television television,
                                                   @PathVariable Long id) {
-        Optional<Television> television = televisionRepository.findById(id);
-
-        if(television.isEmpty()){
-            throw new RecordNotFoundException();
-        } else {
-            Television existingTelevision = television.get();
-
-            existingTelevision.setBrand(newTelevision.getBrand());
-            existingTelevision.setName(newTelevision.getName());
-            existingTelevision.setPrice(newTelevision.getPrice());
-            existingTelevision.setAvailableSize(newTelevision.getAvailableSize());
-            existingTelevision.setRefreshRate(newTelevision.getRefreshRate());
-            existingTelevision.setScreenType(newTelevision.getScreenType());
-            existingTelevision.setScreenQuality(newTelevision.getScreenQuality());
-            existingTelevision.setSmartTv(newTelevision.getSmartTv());
-            existingTelevision.setWifi(newTelevision.getWifi());
-            existingTelevision.setVoiceControl(newTelevision.getVoiceControl());
-            existingTelevision.setHdr(newTelevision.getHdr());
-            existingTelevision.setBluetooth(newTelevision.getBluetooth());
-            existingTelevision.setAmbiLight(newTelevision.getAmbiLight());
-            existingTelevision.setOriginalStock(newTelevision.getOriginalStock());
-            existingTelevision.setSold(newTelevision.getSold());
-
-            Television updatedTelevision = televisionRepository.save(existingTelevision);
-
-            return ResponseEntity.ok().body(updatedTelevision);
+            televisionService.updateTelevision(television, id);
+            return ResponseEntity.noContent().build();
         }
-    }
+
 
     @DeleteMapping("/televisions/{id}")
     public ResponseEntity<Void> deleteTelevision (@PathVariable Long id) {
-        Optional<Television> television = televisionRepository.findById(id);
-        if(television.isEmpty()){
-            throw new RecordNotFoundException();
-        } else {
-            televisionRepository.deleteById(id);
-        }
+        televisionService.deleteTelevision(id);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/example/techiteasybijles/services/TelevisionService.java
+++ src/main/java/com/example/techiteasybijles/services/TelevisionService.java
@@ -1,11 +1,76 @@
 package com.example.techiteasybijles.services;
 
+// De reden dat we de servicelaag gebruiken, is zodat de controller clean blijft
+// Zorgt voor een georganiseerde project structuur, wat bruikbaarheid voor andere programmeurs verbetert
+import com.example.techiteasybijles.exceptions.RecordNotFoundException;
+import com.example.techiteasybijles.models.Television;
 import com.example.techiteasybijles.repositories.TelevisionRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TelevisionService {
 
-    private TelevisionRepository televisionRepository;
+    private final TelevisionRepository televisionRepository;
 
+    public TelevisionService(TelevisionRepository televisionRepository) {
+        this.televisionRepository = televisionRepository;
+    }
+
+    public List<Television> getTelevisions() {
+        return televisionRepository.findAll();
+    }
+
+    // Optional - is empty of is present. Hij geeft altijd wat terug
+    public Television getTelevision(Long id) {
+        Optional<Television> optionalTelevision = televisionRepository.findById(id);
+        if (optionalTelevision.isEmpty()) {
+            throw new RecordNotFoundException("Television not found with id: " + id);
+        } else {
+            return optionalTelevision.get();
+        }
+    }
+
+    public Television saveTelevision(Television television) {
+        televisionRepository.save(television);
+        return television;
+    }
+
+    public void updateTelevision(Television newTelevision, Long id) {
+        Optional<Television> television = televisionRepository.findById(id);
+
+        if (television.isEmpty()) {
+            throw new RecordNotFoundException("Television not found with id: " + id);
+        } else {
+            Television existingTelevision = television.get();
+
+            existingTelevision.setBrand(newTelevision.getBrand());
+            existingTelevision.setName(newTelevision.getName());
+            existingTelevision.setPrice(newTelevision.getPrice());
+            existingTelevision.setAvailableSize(newTelevision.getAvailableSize());
+            existingTelevision.setRefreshRate(newTelevision.getRefreshRate());
+            existingTelevision.setScreenType(newTelevision.getScreenType());
+            existingTelevision.setScreenQuality(newTelevision.getScreenQuality());
+            existingTelevision.setSmartTv(newTelevision.getSmartTv());
+            existingTelevision.setWifi(newTelevision.getWifi());
+            existingTelevision.setVoiceControl(newTelevision.getVoiceControl());
+            existingTelevision.setHdr(newTelevision.getHdr());
+            existingTelevision.setBluetooth(newTelevision.getBluetooth());
+            existingTelevision.setAmbiLight(newTelevision.getAmbiLight());
+            existingTelevision.setOriginalStock(newTelevision.getOriginalStock());
+            existingTelevision.setSold(newTelevision.getSold());
+        }
+    }
+
+    public void deleteTelevision(Long id){
+        Optional<Television> television = televisionRepository.findById(id);
+        if(television.isEmpty()){
+            throw new RecordNotFoundException("Television not found with id: " + id);
+        } else {
+            televisionRepository.deleteById(id);
+        }
+    }
 }
+
